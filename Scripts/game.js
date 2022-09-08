31ce4dffc8e1c231227e8052f6e8276abb0d34e2
erick <erick.sarges0223@gmail.com>
2022-09-08 20:35:21 -0300

    first commit

---
+++ Scripts/game.js
@@ -0,0 +1,75 @@
+const FRONT = "card_front";
+const BACK = "card_back";
+
+
+let techs = [
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react",
+];
+let cards = null;
+startGame();
+
+function startGame(){
+
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+};
+
+function shuffleCards(cards){
+
+    let currentIndex = cards.lenth;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0)
+
+    randomIndex = Math.floor(Math.random * currentIndex);
+
+    currentIndex --;
+
+    [cards[randomIndex],cards[currentIndex]] = [cards[currentIndex],cards[randomIndex]];
+
+};
+
+createCardsFromTechs(techs);
+
+function createCardsFromTechs(techs){
+
+    let cards = [];
+
+    for(let tech of techs){
+
+        cards.push(createPairFromTech(techs))
+    }
+    console.log(cards.flatMap(pair => pair));
+};
+
+function createPairFromTech(tech){
+    return[
+
+        {
+        id: createIdWithTech(tech),
+        icon:tech,
+        flipped:false,
+        },{
+
+        id: createIdWithTech(tech),
+        icon:tech,
+        flipped:false,
+        }];
+
+
+};
+
+function createIdWithTech(tech){
+
+return tech + parseInt(Math.random() *1000);
+
+};
