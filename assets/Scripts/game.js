e6fc58d9058a1a571411604f9f093360ee6f5dbe
erick <erick.sarges0223@gmail.com>
2022-09-12 21:22:01 -0300

    bugs corrigidos

--- assets/Scripts/game.js
+++ assets/Scripts/game.js
@@ -1,5 +1,52 @@
 let game = {
-    techs : [
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+        let card = this.cards.filter(card => card.id === id)[0];
+
+        console.log(card);
+
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true ;
+            return true;
+
+        } else {
+
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        };
+    },
+
+    checkMatch: function () {
+        if(!this.firstCard || !this.secondCard){
+            return false;
+
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+
+    },
+    unflipCards(){
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+
+    },
+
+    techs: [
 
         "bootstrap",
         "css",
@@ -11,57 +58,57 @@ let game = {
         "mongo",
         "node",
         "react",
-        ],
+    ],
 
-        cards: null,
+    cards: null,
 
 
-    createCardsFromTechs:function (techs){
+    createCardsFromTechs: function (techs) {
 
-    this.cards = [];
+        this.cards = [];
 
-    this.techs.forEach((tech)=>{
-    this.cards.push(this.createPairFromTech(tech));
+        this.techs.forEach((tech) => {
+            this.cards.push(this.createPairFromTech(tech));
 
-    })
-this.cards = this.cards.flatMap(pair => pair);
-this.shuffleCards();
-return this.cards;
-},
+        })
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
+        return this.cards;
+    },
 
-createPairFromTech: function(tech){
+    createPairFromTech: function (tech) {
 
-    return [{
+        return [{
 
-        id: this.createIdWithTech(tech),
-        icon:tech,
-        flipped: false,
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false,
 
-    },{
-        id: this.createIdWithTech(tech),
-        icon:tech,
-        flipped: false,
-    }];
-},
+        }, {
+            id: this.createIdWithTech(tech),
+            icon: tech,
+            flipped: false,
+        }];
+    },
 
-createIdWithTech: function(tech){
-    return tech + parseInt(Math.random() *100);
- 
-},
+    createIdWithTech: function (tech) {
+        return tech + parseInt(Math.random() * 1000);
 
-shuffleCards: function(cards){
-    let currentIndex = this.cards.length;
-    let randomIndex = 0;
-    
-    while(currentIndex !== 0){
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-    
-        [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex],this.cards[randomIndex]];
-    
-    
-    };
-    
+    },
+
+    shuffleCards: function (cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
+
+
+        };
+
     },
 };
 
